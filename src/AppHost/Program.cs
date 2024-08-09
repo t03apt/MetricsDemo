1988e0db6fda7489bda6e93c36d0fd2d8c690efd
Tamás Papp <22793739+t03apt@users.noreply.github.com>
2024-08-09 23:21:00 +0200

    add: MetricsDemo

---
+++ src/AppHost/Program.cs
@@ -0,0 +1,22 @@
+﻿var builder = DistributedApplication.CreateBuilder(args);
+
+builder.AddOpenTelemetryCollector("collector", "./otel-collector/config.yaml")
+    .WithEndpoint(8889, 8889)
+    .WithAppForwarding();
+
+var grafana = builder.AddContainer("grafana", "grafana/grafana")
+                     .WithBindMount("./grafana/data/", "/var/lib/grafana")
+                     .WithBindMount("./grafana/config", "/etc/grafana", isReadOnly: true)
+                     .WithBindMount("./grafana/dashboards", "/var/lib/grafana/dashboards", isReadOnly: true)
+                     .WithHttpEndpoint(targetPort: 3000, name: "http");
+
+builder.AddProject<Projects.MetricsApp>("app")
+       .WithEnvironment("GRAFANA_URL", grafana.GetEndpoint("http"));
+
+builder.AddContainer("prometheus", "prom/prometheus")
+       .WithBindMount("./prometheus", "/etc/prometheus", isReadOnly: true)
+       .WithHttpEndpoint(/* This port is fixed as it's referenced from the Grafana config */ port: 9090, targetPort: 9090);
+
+using var app = builder.Build();
+
+await app.RunAsync();
